4bb10b65eedced54e14f9e7ee8e19e42d1c78aeb
G <domok.gergely+github@gmail.com>
2018-04-05 08:48:25 +0200

    Add Book REST methods and views

--- library/app/controllers/books_controller.rb
+++ library/app/controllers/books_controller.rb
@@ -1,2 +1,33 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    update_params = params.permit :title, :author, :isbn, :location
+    book.update update_params
+    book.style = params[:style]
+    book.save!
+
+    redirect_to books_path
+  end
+
+  def create
+    book = Book.new
+    update_params = params.permit :title, :author, :isbn, :location
+    book.update update_params
+    book.style = params[:style]
+    book.save!
+
+    redirect_to books_path
+  end
 end
